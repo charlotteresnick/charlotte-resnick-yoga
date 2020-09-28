9e48ad0b9cca236060e0d2436d7ae7cdec8b1dbb
Charlotte Resnick <charlotte.resnick@gmail.com>
2020-09-28 02:22:17 -0700

    added more heroku build stuff

--- server/server.js
+++ server/server.js
@@ -4,32 +4,49 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 const { PORT } = require("./utils/env");
 
-const app = express();
+const isDev = process.env.NODE_ENV !== "production";
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
-app.use(cookieParser());
-app.use(cors());
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-app.get("/", (_, res) => {
-  res.send("Ok");
-});
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-const apiRouter = require("./routes/api-router");
-app.use("/api", apiRouter);
-
-app.use("*", (_, res) => {
-  return res.status(404).json({
-    message: "not found",
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
   });
-});
+} else {
+  const app = express();
 
-app.listen(PORT || 3001, () => {
-  console.log(`Charlotte Yoga listening on port ${PORT || 3001} 🚀`);
-});
+  app.use(logger("dev"));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(methodOverride("_method"));
+  app.use(express.static("public"));
+  app.use(cookieParser());
+  app.use(cors());
+
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+  const apiRouter = require("./routes/api-router");
+  app.use("/api", apiRouter);
+
+  app.use("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+
+  app.listen(PORT || 3001, () => {
+    console.log(`Charlotte Yoga listening on port ${PORT || 3001} 🚀`);
+  });
+}
